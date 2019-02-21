82607ce0fbaba87d6441a1d655fcac270932b512
MikeJDev <ukmjanes@gmail.com>
2019-02-20 19:58:00 -0600

    added support for cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const bodyParser = require ('body-parser');
 const app = express()
 const port = process.env.PORT || 3000;
 const db = require('../database/dbConnection.js')
-
+const cors = require('cors')
+app.use(cors())
 app.use("/", express.static("dist"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
